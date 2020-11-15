b445aa31b39d774f681dd2c5f71008a6a6998fec
AngelCj1 <70732152+AngelCj1@users.noreply.github.com>
2020-11-15 14:43:48 -0500

    Webbuild

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -7,7 +7,7 @@ public class Hero : MonoBehaviour {
 
     [Header("Set in Inspector")]
     // These fields control the movement of the ship
-    public float speed = 30;
+    public float speed = 35;
     public float rollMult = -45;
     public float pitchMult = 30;
     public float gameRestartDelay = 2f;
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Space-SHMUP
+I altered the speed of the hero. I added a laser in the code, added a menu and tried to add a high score and score count ui but couldn't.
